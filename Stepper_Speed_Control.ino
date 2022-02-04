0deb54732a3d7d9e1c34c0adb648612dd35f4a1c
Jitender Chauhan <66746889+JackEngineer-coder@users.noreply.github.com>
2022-02-04 23:15:16 +0530

    Create Stepper_Speed_Control.ino
    Stepper motor speed control code

---
+++ Stepper_Speed_Control.ino
@@ -0,0 +1,21 @@
+Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+
+int stepCount = 0;  // number of steps the motor has taken
+
+void setup() {
+  // nothing to do inside the setup
+}
+
+void loop() {
+  // read the sensor value:
+  int sensorReading = analogRead(A0);
+  // map it to a range from 0 to 100:
+  int motorSpeed = map(sensorReading, 0, 1023, 0, 100);
+  // set the motor speed:
+  if (motorSpeed > 0) {
+    myStepper.setSpeed(motorSpeed);
+    // step 1/100 of a revolution:
+    myStepper.step(stepsPerRevolution / 100);
+  }
+}
+
